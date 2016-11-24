bbe4e8da28151d57cf3f453cd3d51e64db1f619f
my <1014317271>
2016-11-24 18:01:10 +0800

    first commit

--- app/src/main/java/phone1000/com/advancegit/MainActivity.java
+++ app/src/main/java/phone1000/com/advancegit/MainActivity.java
@@ -3,6 +3,9 @@ package phone1000.com.advancegit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * hello No1
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
